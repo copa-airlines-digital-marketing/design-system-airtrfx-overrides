3afdfdabdc64a57b215791da50a5fbd757e16427
josevch <javch1995@gmail.com>
2023-10-18 15:24:49 -0500

    init for design system repo at Github

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="container mx-auto py-16">
+  <div class="flex !gap-16 md:!gap-24 flex-wrap xs:justify-center sm:justify-center md:justify-center ">
+     <slot />
+  </div>
+      <p class="!font-body-normal !font-sans !text-14/20 !text-center pt-[4]">*Aplican restricciones, consulte nuestros <a
+      href="terminos-y-condiciones#destinoDelMes" target="_blank" rel="noopener"><span class=" !text-primary hover:!underline" >T&eacute;rminos y
+          condiciones</span></a>.</p>
+</div>
